2a6b7e98d13a636c7fd25a9f98ee3ea49d4b4a3c
Julian Homez <julianhomesdev@icloud.com>
2025-11-30 22:23:13 -0500

    Nueva api url de back en railway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://rs28j11l-5174.use2.devtunnels.ms'
+  apiUrl: 'https://firstsaas-production.up.railway.app'
   //http://localhost:5174
-  //'
+  ///
 };
